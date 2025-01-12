61a155fc00332e608605fcb7c48699179d8a9fce
bushlalex <bushlya199920122@gmail.com>
2025-01-12 22:52:06 +0500

    Сделал рисовалку

--- lol.js
+++ lol.js
@@ -8,12 +8,52 @@ function showHowPicsels (valueOfPicsels) {
     let element = document.querySelector('#px');
     element.textContent = valueOfPicsels() +' пикселей';
     element.style.fontSize = 16 + 'px';
+    return valueOfPicsels();
 }
 
 let polzunok = document.querySelector('#razmer');
 polzunok.addEventListener('mousemove', function () {
     showHowPicsels (valueOfPicsels);
 });
+
+function getColor () {
+    return document.querySelector('#color').value
+}
+
+function changeColor () {
+    let element = document.querySelector('#color');
+    element.style.backgroundColor = getColor();
+}
+
+let colorChange = document.querySelector('#color');
+
+colorChange.addEventListener('click', changeColor);
+
+function reload () {
+    location.reload();
+}
+
+let canvas = document.querySelector("canvas");
+let context = canvas.getContext("2d");
+context.fillStyle = getColor ();
+
+
+canvas.addEventListener('mousemove', function() {
+    let x = event.clientX - canvas.offsetLeft;
+    let y = event.clientY - canvas.offsetTop;
+    context.fillStyle = getColor ();
+    context.fillRect(x, y, showHowPicsels (valueOfPicsels), showHowPicsels (valueOfPicsels));
+})
+
+
+
+
+
+
+
+
+
+
 
 
 
